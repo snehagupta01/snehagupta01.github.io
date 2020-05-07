a1f7178e776eaf782f1eb5958670fa3a6c2b39a5
snehagupta01 <sneha2001gupta@gmail.com>
2020-05-07 23:42:46 +0530

    adding onclick effect

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,30 @@
 $(document).ready( function () {
     appendCurvedUnderline();
+    onTileClick();
 });
 function appendCurvedUnderline() {
     const underline = '<div class="underline"> <img src="images/curved.svg"></div>';
     $(".tile-heading").each( function () {
         $(this).append(underline);
     });
+}
+let activeTile = -1;
+let activeScreen = -1;
+function onTileClick () {
+    $(".portion")
+        .on("click", function (e) {
+            if($(this).hasClass("clickable")){
+                activeScreen = 1;
+                activeTile = $(this).index();
+                const removeClassList = "clickable p-inactive p-inactive-"+parseInt(activeTile + 1);
+                const addClassList = "p-active p-active-"+parseInt(activeTile + 1);
+                $(this)
+                    .css({
+                        cursor : "default",
+                        zIndex: 2
+                    })
+                    .removeClass(removeClassList)
+                    .addClass(addClassList);
+            }
+        });
 }
